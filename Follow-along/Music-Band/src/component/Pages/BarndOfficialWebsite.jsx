cc78e315f93fbad236af15f1b059ce1a6b97772e
Heydar Heman <heydar@heman.com>
2021-06-17 11:54:27 +0200

    Added exercises and lessons folder

---
+++ Follow-along/Music-Band/src/component/Pages/BarndOfficialWebsite.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../Pages/Pages.scss"
+
+export const BarndOfficialWebsite = () => {
+  return (
+    <div className="website">
+      
+      <a  href="https://www.pinkfloyd.com" target="_blank" rel="noreferrer" >Pink Floyd Official Website</a>
+      
+    </div>
+  )
+}
---
+++ Follow-along/Music-Band/src/component/Pages/Guitar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import GuitarImg from '../../images/90.jpg'
+
+export const Guitar = () => {
+  return (
+    <div>
+      <h3>GUITAR</h3>
+      <img src={GuitarImg} alt="" />
+    </div>
+  )
+}
